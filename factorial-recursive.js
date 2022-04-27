8021804551534313fc7540e65d5a4de33a490b47
Ahmed Bankole <thegeekyahmed@gmail.com>
2022-04-27 04:23:24 -0700

    learnt factorial method using recursion

---
+++ factorial-recursive.js
@@ -0,0 +1,10 @@
+//Function to get factorial of n using recursion
+
+function getFactorialByResursion(n){
+    if(n === 1) 
+        return n
+    else 
+        return n * getFactorialByResursion(n - 1)
+}
+
+console.log(getFactorialByResursion(4))
--- factorial.js
+++ factorial.js
@@ -1,9 +1,9 @@
 //Function to get the factorial of a given number n
 
-function getFactorial(n){
+function getFactorial(n) {
     let product = 1;
 
-    for(let i = n; i >= 1; i--){
+    for (let i = n; i >= 1; i--) {
         product *= i
     }
 
